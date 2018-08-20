bfc3d3092e38c32bd227c5c2c3966f3a2790d937
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-20 23:13:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft();
   }
-  if (e.which === 36) {
+  if (e.which === 39) {
     moveDodgerRight();
   }
 });
