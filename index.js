37d59b347855e1e2555d514203791e2915ae495a
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-20 23:06:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function moveDodgerRight() {
   var rightNumbers = dodger.style.left.replace('px', '');
   var right = parseInt(rightNumbers, 10);
  
-  if (right > 0) {
+  if (right < 400) {
     dodger.style.left = `${left + 1}px`;
   }
 }
